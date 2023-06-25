baf6c8b8f15b0d8b35c5fad291af39f1e07e7da2
Zao0824 <925889398@qq.com>
2023-06-24 22:26:45 -0700

    Finished parking lot design section

---
+++ Coding/ParkingLotSimulation/src/main/java/org/example/Bike.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Bike implements Vehicle {
+    private String bikeNumber;
+
+    public Bike(String bikeNumber) {
+        this.bikeNumber = bikeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "bike number=" + bikeNumber;
+    }
+
+    @Override
+    public VehicleType getType() {
+        return VehicleType.BIKE;
+    }
+}
