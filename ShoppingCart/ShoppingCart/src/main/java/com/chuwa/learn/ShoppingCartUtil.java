1dea6fba7aac33a48f0db995bb76961b248b979e
Wei Wang <62wwang@gmail.com>
2023-07-08 16:29:19 -0400

    hw23

---
+++ ShoppingCart/ShoppingCart/src/main/java/com/chuwa/learn/ShoppingCartUtil.java
@@ -0,0 +1,71 @@
+package com.chuwa.learn;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+public class ShoppingCartUtil {
+    private static final List<String> ALTERNATIVE_ITEMS = Arrays.asList("Apple", "Orange", "Banana", "Grape");
+    public static Optional<String> getFirstItemName(Customer customer){
+        return Optional.ofNullable(customer)
+                .map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems)
+                .filter(items -> !items.isEmpty())
+                .map(items -> items.get(0))
+                .map(Item::getName);
+    }
+    public static double getTotalPrice(Optional<Customer> customer){
+        return customer.map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems)
+                .filter(items -> !items.isEmpty())
+                .map(items -> items.stream().mapToDouble(Item::getPrice).sum())
+                .orElse(0.0);
+    }
+    public static String getFirstItemNameWithAlternative(Customer customer){
+        return Optional.ofNullable(customer)
+                .map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems)
+                .filter(items -> !items.isEmpty())
+                .map(items -> items.get(0))
+                .map(Item::getName)
+                .orElseGet(ShoppingCartUtil::getRandomAlternativeItem);
+    }
+    public static String getFirstItemNameOrThrowException(Customer customer) throws EmptyCartException {
+        return Optional.ofNullable(customer)
+                .map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems)
+                .filter(items -> !items.isEmpty())
+                .map(items -> items.get(0))
+                .map(Item::getName)
+                .orElseThrow(() -> new EmptyCartException("Empty Cart"));
+    }
+
+    public static void checkItemsInCart(Customer customer) {
+        Optional<List<Item>> itemOptional = Optional.ofNullable(customer)
+                .map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems);
+        if(itemOptional.isPresent() && !itemOptional.get().isEmpty()){
+            System.out.println("购物车中有商品");
+        }
+        else{
+            System.out.println("购物车为空");
+        }
+    }
+    public static void printItemsInCart(Customer customer) {
+        Optional<List<Item>> itemOptional = Optional.ofNullable(customer)
+                .map(Customer::getShoppingCart)
+                .map(ShoppingCart::getItems);
+        itemOptional.ifPresent(items -> {
+            if(!items.isEmpty()){
+                System.out.println("Cart has:");
+                items.forEach(item -> System.out.println(item.getName()));
+            }
+        });
+    }
+    public static String getRandomAlternativeItem() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(ALTERNATIVE_ITEMS.size());
+        return ALTERNATIVE_ITEMS.get(randomIndex);
+    }
+}
