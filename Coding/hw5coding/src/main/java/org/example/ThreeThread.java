983540d9328a91643c7ee55274f9daf3a2fd834e
Yesibao Muhamaiti <myesibao3@gatech.edu>
2023-07-14 03:16:41 -0400

    finished hw5

---
+++ Coding/hw5coding/src/main/java/org/example/ThreeThread.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class ThreeThread {
+    public static int n = 1;
+
+    private static synchronized void printNumber() {
+        int count = 10;
+        while (count > 0) {
+            System.out.println(Thread.currentThread().getName() + ": " + n++);
+            count--;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> printNumber());
+        Thread t2 = new Thread(() -> printNumber());
+        Thread t3 = new Thread(() -> printNumber());
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
