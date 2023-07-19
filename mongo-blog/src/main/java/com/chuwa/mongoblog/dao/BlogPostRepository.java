125903f1fe6deae3d63be4fd2104fb918582cac2
Chingpo-Lin <m13823601165@163.com>
2023-07-18 17:09:44 -0700

    finish hw41 complete

--- mongo-blog/src/main/java/com/chuwa/mongoblog/dao/BlogPostRepository.java
+++ mongo-blog/src/main/java/com/chuwa/mongoblog/dao/BlogPostRepository.java
@@ -2,6 +2,8 @@ package com.chuwa.mongoblog.dao;
 
 import com.chuwa.mongoblog.entity.BlogPost;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogPostRepository extends MongoRepository<BlogPost, String> {
 }
--- redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
+++ redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
@@ -4,9 +4,13 @@ import com.chuwa.redbook.dao.PostRepository;
 import com.chuwa.redbook.entity.Post;
 import com.chuwa.redbook.exception.ResourceNotFoundException;
 import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
 import com.chuwa.redbook.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -63,6 +67,25 @@ public class PostServiceImpl implements PostService {
         Post post = postRepository.findById(id).orElseThrow(() ->
                 new ResourceNotFoundException("post not found", "post with", id));
         postRepository.delete(post);
+    }
+
+    @Override
+    public PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Post> pagePosts = postRepository.findAll(pageRequest);
+
+        List<Post> posts = pagePosts.getContent();
+        List<PostDto> postDtos = posts.stream().map(post -> changeToPostDto(post)).collect(Collectors.toList());
+
+        return new PostResponse(postDtos, pagePosts.getNumber(),
+                        pagePosts.getSize(), pagePosts.getTotalElements(),
+                        pagePosts.getTotalPages(), pagePosts.isLast());
+
     }
 
     private PostDto changeToPostDto(Post post) {
