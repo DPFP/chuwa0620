0cdb08d9aac945d88b301651b0eb70351dd241fe
lichu <lj2231@nyu.edu>
2023-07-20 22:30:57 -0400

    finished hw40.2

---
+++ Coding/redbook/src/main/java/com/chuwa/redbook/entity/Author.java
@@ -0,0 +1,59 @@
+package com.chuwa.redbook.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(name = "first_name", nullable = false)
+    String firstName;
+    @Column(name = "last_name", nullable = false)
+    String lastName;
+    @Column(name = "age", nullable = false)
+    int age;
+
+    public Author() {
+    }
+
+    public Author(long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
