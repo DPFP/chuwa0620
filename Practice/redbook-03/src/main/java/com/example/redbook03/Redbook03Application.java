76d04fab25cbe278571378eaeeebc78d16f08198
LeremyLin <jeremy0077jj@gmail.com>
2023-07-20 21:29:47 -0700

    hw9 part3 - coding

---
+++ Practice/redbook-03/src/main/java/com/example/redbook03/Redbook03Application.java
@@ -0,0 +1,13 @@
+package com.example.redbook03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redbook03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redbook03Application.class, args);
+    }
+
+}
---
+++ Practice/redbook-03/src/main/java/com/example/redbook03/dao/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.redbook03.dao;
+
+import com.example.redbook03.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
