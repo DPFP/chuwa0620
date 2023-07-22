aedaf5606b548de240cc879c5fdc1ae3b480ef8e
Jiaq1Zhu <i@zjiaqi.com>
2023-07-21 22:34:43 -0500

    Complete hw9 comment feature

---
+++ MavenProject/redbook/src/main/java/com/chuwa/redbook/payload/CommentDto.java
@@ -0,0 +1,16 @@
+package com.chuwa.redbook.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CommentDto {
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+
+}
