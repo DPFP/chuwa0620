10a5e7548376ad7d84b206e7f1bbbdc300772bed
dtxpder <1119753960@qq.com>
2023-07-23 00:14:10 -0400

    finish redbook comment

---
+++ MavenProject/redbook/src/main/java/com/chuwa/redbook/controller/PostController.java
@@ -0,0 +1,57 @@
+package com.chuwa.redbook.controller;
+
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
+import com.chuwa.redbook.service.PostService;
+import com.chuwa.redbook.util.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+
+        return new ResponseEntity<>(postService.createPost(postDto),
+                HttpStatus.CREATED);
+    }
+
+//    @GetMapping
+//    public ResponseEntity<List<PostDto>> getAllPosts(){
+//        return new ResponseEntity<>(postService.getAllPost(), HttpStatus.ACCEPTED);
+//    }
+
+    @GetMapping()
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir
+    ) {
+        return postService.getAllPost(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
+        return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@PathVariable(name = "id") long id, @RequestBody PostDto postDto){
+        return ResponseEntity.ok(postService.updatePost(postDto, id));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("deleted id " + id, HttpStatus.OK);
+    }
+}
