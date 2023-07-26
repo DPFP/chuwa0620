e65aa212a1bd1ee9e4e7007e63296d49c734d02a
Chingpo-Lin <m13823601165@163.com>
2023-07-26 14:52:43 -0700

    complete hw43

---
+++ ShortQuestions/hw43/MyBean.java
@@ -0,0 +1,11 @@
+public class MyBean {
+    private String name;
+    private int age;
+
+    public MyBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and setters
+}
